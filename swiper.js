06e39673fce094946b00a82aa45fd4bf4413af1d
Sam Burke <74158978+samburke97@users.noreply.github.com>
2021-08-02 13:44:14 +1000

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    centeredSlides: true,
+    mousewheel: true,
+    keyboard: true,
+  });
